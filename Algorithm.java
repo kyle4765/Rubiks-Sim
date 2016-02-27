57a4fc8f4316eba98eaeed9a9b9cc0bed2b8b993
kyle4765 <kg4765@gmail.com>
2016-02-26 21:18:36 -0500

    Added files via upload

---
+++ Algorithm.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.applet.*;																
+import java.util.*;
+
+public class Algorithm
+{
+   protected String actions;
+   protected String inverses;
+   
+   public Algorithm()
+   {
+      actions = "";
+      inverses = "";
+   }
+   public Algorithm(String act, String inv)
+   {
+      actions = act;
+      inverses = inv;
+   }
+   
+   public void setActions(String act)  {actions = act;}
+   public void setInverses(String inv) {inverses = inv;}
+   
+   public String getActions()  {return actions;}
+   public String getInverses() {return inverses;}
+}
