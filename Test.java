409805cfd5334a9ebcef70daf88f67c7ef4555ce
Max Rosen <mfrosen100@gmail.com>
2016-03-02 11:59:09 -0500

    Update Test.java
    Added KeyListener

--- Test.java
+++ Test.java
@@ -1,12 +1,17 @@
 import java.awt.*;
 import java.applet.*;																
 import java.util.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter; 
 
-public class Test extends Applet
+
+public class Test extends Applet implements KeyListener
 {
    public void paint(Graphics g)
    //public static void main(String[]args)
    {
+      this.setFocusable(true);
+      
       Rubiks cube = new Rubiks();
       
       cube.L(10);
