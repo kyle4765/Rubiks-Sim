ef31f04df0a1d7b6a281ffc909576503e3cea650
kyle4765 <kg4765@gmail.com>
2016-03-04 08:10:51 -0500

    Update WhiteCross.java

--- WhiteCross.java
+++ WhiteCross.java
@@ -1,19 +1,37 @@
-public class WhiteCross
+public class WhiteCross extends Solver
 {
    final Algorithm fromBottom    = new Algorithm("FF","00");
+   final Algorithm fromLeft      = new Algorithm("F", "0");
+   final Algorithm fromRight     = new Algorithm("F", "1");
    final Algorithm fromLeftFix   = new Algorithm("URU", "100");
-   final Algorithm fromrightFix  = new Algorithm("ULU", "011");
+   final Algorithm fromRightFix  = new Algorithm("ULU", "011");
    final Algorithm fromBottomFix = new Algorithm("UFRFU", "11000");
+   final Algorithm fromTopFix    = new Algorithm("ULUF","0010");
+   
    
    protected int[] edgeLoc = {1,3,5,7};
    protected boolean[] edgeState = {false, false, false, false};
    
    
+   public void Solve(Rubiks cube)
+   {
+      setFace(cube, "blue", "white");
+      boolean isDone;
+      do
+      {
+      
+      isDone = checkComplete(cube);
+      }
+      while(!isDone);
+      // mas
+   }
    
+   public void firstStep(Rubiks cube)
+   {
+      
+   }
    
-   
-   
-   public void checkComplete(Rubiks cube)
+   public boolean checkComplete(Rubiks cube)
    {
       boolean temp = false;
       for (int k = 0; k < 4; k++)
@@ -28,5 +46,12 @@ public class WhiteCross
       temp = temp && cube.getU()[edgeLoc[k]].equals(cube.getU()[4]);
       edgeState[k] = temp;
       }
+      for (boolean edge : edgeState)
+      {
+         if (!edge)
+            return false;
+      }
+      return true;
+            
    }
 }
