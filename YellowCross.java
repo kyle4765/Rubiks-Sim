7558ecc3c29948da7997aace2073ce126bd30008
kyle4765 <kg4765@gmail.com>
2016-05-11 08:22:49 -0400

    Completed yellow cross solving
    Not thoroughly tested, but the yellow cross solving appears to be fully functional

--- YellowCross.java
+++ YellowCross.java
@@ -1,23 +1,51 @@
+import java.awt.*;
+
 public class YellowCross extends Solver
 {
-   final Algorithm curve   = new Algorithm("FURURF", "000111");
-   final Algorithm line    = new Algorithm("FRURUF", "000111");
+   final static Algorithm curve   = new Algorithm("FURURF", "000111");
+   final static Algorithm line    = new Algorithm("FRURUF", "000111");
    
-   public void Solve(Rubiks cube)
+   public static void solve(Rubiks cube)
    {
-      setFace(cube, "blue", "yellow");
-      boolean isDone;
-      do
-      {
-      isDone = checkComplete(cube);
-      }
-      while(!isDone);
+	   cube.setFace("blue", "yellow");
+	   doAlgorithms(cube, findState(cube));
    }
    
-   public boolean checkComplete(Rubiks cube)
+   public static int findState(Rubiks cube)
    {
+	   if (  (cube.getU()[1].equals(Color.yellow)) && (cube.getU()[3].equals(Color.yellow)) &&
+			 (cube.getU()[5].equals(Color.yellow)) && (cube.getU()[7].equals(Color.yellow)) )
+		   return 0;
+	   if (!((cube.getU()[1].equals(Color.yellow)) || (cube.getU()[3].equals(Color.yellow)) ||
+		     (cube.getU()[5].equals(Color.yellow)) || (cube.getU()[7].equals(Color.yellow))) )
+		   return 1;
+	   for (int k = 0; k < 4; k++)
+	   {
+		   if (cube.getU()[3].equals(Color.yellow))
+		   {
+			   if(cube.getU()[1].equals(Color.yellow))
+			   {
+				   return 2;
+			   }
+			   else if (cube.getU()[5].equals(Color.yellow))
+			   {
+				   return 3;
+			   }
+		   }
+		   cube.U(0);
+	   }
+	   System.out.println("Something in the yellow cross is whack");
+	   return -1;     
+   }
    
-      return true;
-            
+   public static void doAlgorithms(Rubiks cube, int state)
+   {
+	   switch (state)
+	   {
+	   case 0 : break;
+	   case 1 : cube.algorithm(curve); cube.U(0); cube.algorithm(line); break;
+	   case 2 : cube.algorithm(curve); break;
+	   case 3 : cube.algorithm(line);; break;
+	   }
    }
 }
