4d2644cd3260a3f186f5de8ff51562b1b1616193
kyle4765 <kg4765@gmail.com>
2016-05-12 11:02:32 -0400

    Update YellowFace.java

--- YellowFace.java
+++ YellowFace.java
@@ -1,22 +1,59 @@
+import java.awt.Color;
+
 public class YellowFace extends Solver
 {
-   final Algorithm corners = new Algorithm("RURURUUR", "00100001");
+   final static Algorithm normCorners = new Algorithm("R-U-R-U-R-UU-R", "0-0-1-0-0-00-1");
+   final static Algorithm invCorners  = new Algorithm("RUURURUR", "00011010");
    
-   public void Solve(Rubiks cube)
+   public static void solve(Rubiks cube)
    {
-      setFace(cube, "blue", "yellow");
-      boolean isDone;
-      do
-      {
-      isDone = checkComplete(cube);
-      }
-      while(!isDone);
+	   cube.setFace("blue", "yellow");
+	   //while (findState(cube) != 0)
+	   for (int tries = 0; tries < 100; tries++)
+		   doAlgorithms(cube, findState(cube));
    }
    
-   public boolean checkComplete(Rubiks cube)
+   public static int findState(Rubiks cube)
    {
-   
-      return true;
-            
+	   Color y = Color.yellow;
+	   for (int k = 0; k < 4; k++)
+	   {
+		   if (  (cube.getU(0).equals(y)) && (cube.getU(2).equals(y)) &&
+			     (cube.getU(6).equals(y)) && (cube.getU(8).equals(y)) )
+				   return 0;
+		   else if (cube.getR(0).equals(y) && cube.getL(2).equals(y))
+			   return 1;
+		   else if (cube.getU(6).equals(y) && cube.getR(0).equals(y) &&
+				    cube.getB(0).equals(y) && cube.getL(0).equals(y))
+			   return 1;
+		   else if (cube.getU(6).equals(y) && cube.getF(2).equals(y) &&
+				    cube.getB(2).equals(y) && cube.getR(2).equals(y))
+			   return 1;
+		   else if (cube.getU(0).equals(y) && cube.getU(8).equals(y) &&
+				    cube.getL(2).equals(y) && cube.getB(0).equals(y))
+			   return 1;
+		   else if (cube.getU(0).equals(y) && cube.getU(2).equals(y) &&
+				    cube.getF(2).equals(y) && cube.getF(0).equals(y))
+			   return 1;
+		   cube.U(0);
+	   }
+	   
+	   
+	   System.out.println("There's something rotten in Denmark");
+	   return -1;   
    }
+   
+   public static void doAlgorithms(Rubiks cube, int state)
+   {
+	   switch (state)
+	   {
+	   case 0  : break;
+	   case 1  : cube.algorithm(normCorners); break;
+	   case 2  : cube.algorithm(invCorners); break;
+	   case -1 : break;
+
+	   }
+   }
+   
+
 }
