1b4eb8cbd2dab9e591977b9a5df2f421ad214aa9
kyle4765 <kg4765@gmail.com>
2016-05-20 08:22:37 -0400

    Got the target solving to work

--- YellowCross.java
+++ YellowCross.java
@@ -30,63 +30,67 @@ public class YellowCross extends Solver
    
    public static int findState(Rubiks cube, Rubiks target) throws IOException
    {
-	   if (((cube.getU(1) == target.getU(1) && cube.getB(1) == target.getB(1)) ||
-			(cube.getU(1) == target.getU(2) && cube.getB(1) == target.getL(1)) ||
-			(cube.getU(1) == target.getU(5) && cube.getB(1) == target.getR(1)) ||
-			(cube.getU(1) == target.getU(7) && cube.getB(1) == target.getF(1)))&&
+	   if ( ((cube.getU(1) == target.getU(1) && cube.getB(1) == target.getB(1)) ||
+			 (cube.getU(1) == target.getU(3) && cube.getB(1) == target.getL(1)) ||
+			 (cube.getU(1) == target.getU(5) && cube.getB(1) == target.getR(1)) ||
+			 (cube.getU(1) == target.getU(7) && cube.getB(1) == target.getF(1)))&&
 			
-		   ((cube.getU(3) == target.getU(1) && cube.getL(1) == target.getB(1)) ||
-		    (cube.getU(3) == target.getU(2) && cube.getL(1) == target.getL(1)) ||
-		    (cube.getU(3) == target.getU(5) && cube.getL(1) == target.getR(1)) ||
-			(cube.getU(3) == target.getU(7) && cube.getL(1) == target.getF(1)))&&
+		    ((cube.getU(3) == target.getU(1) && cube.getL(1) == target.getB(1)) ||
+		     (cube.getU(3) == target.getU(3) && cube.getL(1) == target.getL(1)) ||
+		     (cube.getU(3) == target.getU(5) && cube.getL(1) == target.getR(1)) ||
+			 (cube.getU(3) == target.getU(7) && cube.getL(1) == target.getF(1)))&&
 			
-		   ((cube.getU(5) == target.getU(1) && cube.getR(1) == target.getB(1)) ||
-		    (cube.getU(5) == target.getU(2) && cube.getR(1) == target.getL(1)) ||
-			(cube.getU(5) == target.getU(5) && cube.getR(1) == target.getR(1)) ||
-			(cube.getU(5) == target.getU(7) && cube.getR(1) == target.getF(1)))&&
+		    ((cube.getU(5) == target.getU(1) && cube.getR(1) == target.getB(1)) ||
+		     (cube.getU(5) == target.getU(3) && cube.getR(1) == target.getL(1)) ||
+			 (cube.getU(5) == target.getU(5) && cube.getR(1) == target.getR(1)) ||
+			 (cube.getU(5) == target.getU(7) && cube.getR(1) == target.getF(1)))&&
 					
-		   ((cube.getU(7) == target.getU(1) && cube.getF(1) == target.getB(1)) ||
-		    (cube.getU(7) == target.getU(2) && cube.getF(1) == target.getL(1)) ||
-			(cube.getU(7) == target.getU(5) && cube.getF(1) == target.getR(1)) ||
-			(cube.getU(7) == target.getU(7) && cube.getF(1) == target.getF(1))) )
+		    ((cube.getU(7) == target.getU(1) && cube.getF(1) == target.getB(1)) ||
+		     (cube.getU(7) == target.getU(3) && cube.getF(1) == target.getL(1)) ||
+			 (cube.getU(7) == target.getU(5) && cube.getF(1) == target.getR(1)) ||
+			 (cube.getU(7) == target.getU(7) && cube.getF(1) == target.getF(1))) )
+	   {
 		   return 0;
+	   }
 	   
-	   if(!((cube.getU(1) == target.getU(1) && cube.getB(1) == target.getB(1)) ||
-			(cube.getU(1) == target.getU(2) && cube.getB(1) == target.getL(1)) ||
-			(cube.getU(1) == target.getU(5) && cube.getB(1) == target.getR(1)) ||
-			(cube.getU(1) == target.getU(7) && cube.getB(1) == target.getF(1)))||
+	   if(!(((cube.getU(1) == target.getU(1) && cube.getB(1) == target.getB(1)) ||
+			 (cube.getU(1) == target.getU(3) && cube.getB(1) == target.getL(1)) ||
+			 (cube.getU(1) == target.getU(5) && cube.getB(1) == target.getR(1)) ||
+			 (cube.getU(1) == target.getU(7) && cube.getB(1) == target.getF(1)))||
 				
-		   ((cube.getU(3) == target.getU(1) && cube.getL(1) == target.getB(1)) ||
-			(cube.getU(3) == target.getU(2) && cube.getL(1) == target.getL(1)) ||
-			(cube.getU(3) == target.getU(5) && cube.getL(1) == target.getR(1)) ||
-		    (cube.getU(3) == target.getU(7) && cube.getL(1) == target.getF(1)))||
+		    ((cube.getU(3) == target.getU(1) && cube.getL(1) == target.getB(1)) ||
+			 (cube.getU(3) == target.getU(3) && cube.getL(1) == target.getL(1)) ||
+			 (cube.getU(3) == target.getU(5) && cube.getL(1) == target.getR(1)) ||
+		     (cube.getU(3) == target.getU(7) && cube.getL(1) == target.getF(1)))||
 				
-		   ((cube.getU(5) == target.getU(1) && cube.getR(1) == target.getB(1)) ||
-			(cube.getU(5) == target.getU(2) && cube.getR(1) == target.getL(1)) ||
-	    	(cube.getU(5) == target.getU(5) && cube.getR(1) == target.getR(1)) ||
-		    (cube.getU(5) == target.getU(7) && cube.getR(1) == target.getF(1)))||
+		    ((cube.getU(5) == target.getU(1) && cube.getR(1) == target.getB(1)) ||
+			 (cube.getU(5) == target.getU(3) && cube.getR(1) == target.getL(1)) ||
+	    	 (cube.getU(5) == target.getU(5) && cube.getR(1) == target.getR(1)) ||
+		     (cube.getU(5) == target.getU(7) && cube.getR(1) == target.getF(1)))||
 						
-		   ((cube.getU(7) == target.getU(1) && cube.getF(1) == target.getB(1)) ||
-			(cube.getU(7) == target.getU(2) && cube.getF(1) == target.getL(1)) ||
-		    (cube.getU(7) == target.getU(5) && cube.getF(1) == target.getR(1)) ||
-			(cube.getU(7) == target.getU(7) && cube.getF(1) == target.getF(1))) )
-			   return 1;
+		    ((cube.getU(7) == target.getU(1) && cube.getF(1) == target.getB(1)) ||
+		 	 (cube.getU(7) == target.getU(3) && cube.getF(1) == target.getL(1)) ||
+		     (cube.getU(7) == target.getU(5) && cube.getF(1) == target.getR(1)) ||
+			 (cube.getU(7) == target.getU(7) && cube.getF(1) == target.getF(1)))))
+	   {
+		   return 1;
+	   }
 	   for (int k = 0; k < 4; k++)
 	   {
 		   if (((cube.getU(3) == target.getU(1) && cube.getL(1) == target.getB(1)) ||
-			    (cube.getU(3) == target.getU(2) && cube.getL(1) == target.getL(1)) ||
+			    (cube.getU(3) == target.getU(3) && cube.getL(1) == target.getL(1)) ||
 				(cube.getU(3) == target.getU(5) && cube.getL(1) == target.getR(1)) ||
 				(cube.getU(3) == target.getU(7) && cube.getL(1) == target.getF(1))) )
 		   {
 			   if (((cube.getU(1) == target.getU(1) && cube.getB(1) == target.getB(1)) ||
-					(cube.getU(1) == target.getU(2) && cube.getB(1) == target.getL(1)) ||
+					(cube.getU(1) == target.getU(3) && cube.getB(1) == target.getL(1)) ||
 					(cube.getU(1) == target.getU(5) && cube.getB(1) == target.getR(1)) ||
 					(cube.getU(1) == target.getU(7) && cube.getB(1) == target.getF(1))) )
 			   {
 				   return 2;
 			   }
 			   else if(((cube.getU(5) == target.getU(1) && cube.getR(1) == target.getB(1)) ||
-						(cube.getU(5) == target.getU(2) && cube.getR(1) == target.getL(1)) ||
+						(cube.getU(5) == target.getU(3) && cube.getR(1) == target.getL(1)) ||
 						(cube.getU(5) == target.getU(5) && cube.getR(1) == target.getR(1)) ||
 						(cube.getU(5) == target.getU(7) && cube.getR(1) == target.getF(1))) )
 			   {
@@ -101,6 +105,7 @@ public class YellowCross extends Solver
    
    public static void doAlgorithms(Rubiks cube, int state) throws IOException
    {
+	   System.out.println(state);
 	   switch (state)
 	   {
 	   case 0 : break;
